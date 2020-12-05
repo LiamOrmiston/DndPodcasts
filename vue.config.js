81ca2e2ecf4ebef34f6bd8f177559d9adb70919f
Liam Ormiston <ljormiston@ku.edu>
2020-12-05 14:16:36 -0600

    updated dependencies. changed title via config file instead.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Dnd Podcasts',
+    }
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/DndPodcasts/'
     : '/'
